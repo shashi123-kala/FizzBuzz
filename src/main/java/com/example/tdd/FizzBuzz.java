3ff843877c06da97be7ae3c88b11ec157aa1ba82
LENOVO <LENOVO@DESKTOP-2MREFRI>
2023-05-10 20:48:35 +0530

    start the game

--- src/main/java/com/example/tdd/FizzBuzz.java
+++ src/main/java/com/example/tdd/FizzBuzz.java
@@ -2,5 +2,14 @@ package com.example.tdd;
 
 public class FizzBuzz {
 
-	
+public String play(int number) {		
+		
+		if(number % 3 ==0 ) 
+			return "Fizz";		
+		if(number % 5 == 0 ) 
+			return "Buzz";		
+			return String.valueOf(number);
+	}
+
+
 }
--- src/test/java/com/example/tdd/FizzBuzzTest.java
+++ src/test/java/com/example/tdd/FizzBuzzTest.java
@@ -2,14 +2,24 @@ package com.example.tdd;
 
 import static org.junit.Assert.assertNotNull;
 
+import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
 
 public class FizzBuzzTest {
 	
+	private  FizzBuzz fizzBuz;
+	@Before
+	public void setup() {
+		 fizzBuz = new FizzBuzz();
+	}
+	
 	@Test
 	public void testNumber() {
-		FizzBuzz fizzBuzz = new FizzBuzz();
-		assertNotNull( fizzBuzz );
+		int number = 1;
+		String numberReturned = fizzBuz.play(number);
+		Assert.assertEquals("1",numberReturned );
+		
 	}
 
 }
